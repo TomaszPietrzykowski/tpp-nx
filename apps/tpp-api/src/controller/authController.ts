05cf0e0a3cdf89ba552092cc0f6e83c2f9a37af2
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2023-11-06 21:44:46 +0100

    minor

--- apps/tpp-api/src/controller/authController.ts
+++ apps/tpp-api/src/controller/authController.ts
@@ -1,7 +1,7 @@
 import User from '../model/userModel';
 import catchAsync from '../utils/catchAsync';
 
-export const register = catchAsync(async (req, res, next) => {
+export const register = catchAsync(async (req, res) => {
   const newUser = await User.create(req.body);
 
   res.status(201).json({
--- apps/tpp-api/src/model/userModel.ts
+++ apps/tpp-api/src/model/userModel.ts
@@ -1,47 +1,50 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Podaj swoje imię'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Podaj swój adres email'],
-    unique: true,
-    lowercase: true,
-    validate: {
-      validator: function (v) {
-        return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(v);
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Podaj swoje imię'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Podaj swój adres email'],
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(v);
+        },
+        message: (props) =>
+          `The email '${props.value}' in the field '${props.path}' is not valid!`,
       },
-      message: (props) =>
-        `The email '${props.value}' in the field '${props.path}' is not valid!`,
+    },
+    avatar: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isAuthor: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    password: {
+      type: String,
+      required: [true, 'Podaj hasło'],
+      minlength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Potwierdź hasło'],
+      minlength: 8,
     },
   },
-  avatar: {
-    type: String,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  isAuthor: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  password: {
-    type: String,
-    required: [true, 'Podaj hasło'],
-    minlength: 8,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Potwierdź hasło'],
-    minlength: 8,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
